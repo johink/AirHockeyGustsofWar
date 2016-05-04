d75886c3485ae8c6b4be5ccfdc81f86d9ec9d4f1
mtennis@gru.edu <mtennis@gru.edu>
2016-05-04 00:07:50 -0400

    MainMenuActivity cannibalized into TitleActivity. Removed extra animation elements, click functionality to the logo ImageView,, and made it look pretty.

--- app/src/main/java/onyeka/is/great/airhockeygustsofwar/TitleActivity.java
+++ app/src/main/java/onyeka/is/great/airhockeygustsofwar/TitleActivity.java
@@ -1,19 +1,17 @@
 package onyeka.is.great.airhockeygustsofwar;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -24,6 +22,7 @@ public class TitleActivity extends AppCompatActivity {
     Context context;
     ViewGroup titleLayoutGroup;
     LinearLayout mainMenuLayout;
+    Button btnSingle, btnLocal, btnOnline, btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +33,18 @@ public class TitleActivity extends AppCompatActivity {
         final ImageView logo = (ImageView) findViewById(R.id.logoView);
         titleLayoutGroup = (ViewGroup) findViewById(R.id.titleLayout);
         mainMenuLayout = (LinearLayout) findViewById(R.id.mainMenuLayout);
-        // AnimationSet animationSet = new AnimationSet(true);
-        // AnimatorSet animatorSet = new AnimatorSet();
-        // AnimatorInflater animatorInflater = new AnimatorInflater();
+
+        // MainMenuActivity.onCreate()
+        btnSingle = (Button) findViewById(R.id.btnSingle);
+        btnLocal = (Button) findViewById(R.id.btnLocal);
+        btnOnline = (Button) findViewById(R.id.btnOnline);
+        btnShare = (Button) findViewById(R.id.btnShare);
+        // end MainMenuActivity.onCreate()
 
         final Animation fadeinAnimation =
                 AnimationUtils.loadAnimation(this, R.anim.logo_fadein_anim); // title logo fades in
         final Animation menu_fadeInAnimation =
                 AnimationUtils.loadAnimation(this, R.anim.mainmenu_fadein_anim); // main menu fades in
-
-/*      animationSet.addAnimation(fadeinAnimation);
-        animationSet.addAnimation(scrollUpAnimation);
-        animationSet.addAnimation(menu_fadeInAnimation);*/
 
         fadeinAnimation.setAnimationListener(new Animation.AnimationListener() {
              @Override
@@ -66,40 +65,50 @@ public class TitleActivity extends AppCompatActivity {
 
              }
          });
-/*
-        menu_fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+        View.OnClickListener localListener = new View.OnClickListener() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
+            public void onClick(View v) {
+                Intent mIntent = new Intent(context, MainActivity.class);
+                startActivity(mIntent);
             }
+        };
 
+        View.OnClickListener shareListener = new View.OnClickListener() {
             @Override
-            public void onAnimationEnd(Animation animation) {
-
+            public void onClick(View v) {
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                smsIntent.setType("vnd.android-dir/mms-sms");
+                smsIntent.putExtra("address", "8675309");
+                smsIntent.putExtra("sms_body","Hey, check out this game!  \"Air Hockey:  Gusts of War\", available on the Google Play Store!");
+                startActivity(smsIntent);
             }
+        };
 
+        View.OnLongClickListener playerListener = new View.OnLongClickListener() {
             @Override
-            public void onAnimationRepeat(Animation animation) {
-
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                return true;
             }
-        });
-*/
+        };
+
+        btnLocal.setOnClickListener(localListener);
+        btnShare.setOnClickListener(shareListener);
 
         logo.startAnimation(fadeinAnimation);
         mainMenuLayout.startAnimation(menu_fadeInAnimation);
 
-        View.OnClickListener listener = new View.OnClickListener() {
+/*        View.OnClickListener logoListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mainMenu();
             }
         };
-
-        logo.setOnClickListener(listener);
-        //logo.setOnClickListener(listener);
+        logo.setOnClickListener(logoListener);*/ // dont need to click on logo anymore
     }
 
-
+    // deprecated
     public void mainMenu()
     {
         Intent mIntent = new Intent(context, MainMenuActivity.class);
